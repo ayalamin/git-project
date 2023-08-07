1bea27b1c63fe0b64ca7c5742558312cda3e12d5
Ayala Min Hahar <ayala59020@gmail.com>
2023-08-07 16:22:38 +0300

    in the middel ex2

--- Exercise_2/ex2.sh
+++ Exercise_2/ex2.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+oldIFS=$IFS
+IFS=';' 
+
+divide_saperate() {
+read -ra fields <<< "$@"
+fields_length=${#fields[@]}
+for (( i = 0; i < fields_length; i++)); do
+    echo "field $((i + 1)) : ${fields[i]}"
+done
+
+}
+
+divide_saperate
+
+
+IFS="$oldIFS" 
