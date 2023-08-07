9a13f357c1e59103c98f2562965a1017a92d20c2
Ayala Min Hahar <ayala59020@gmail.com>
2023-08-07 17:50:21 +0300

    now I finish ex2 completely

--- Exercise_2/ex2.sh
+++ Exercise_2/ex2.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-oldIFS=$IFS
-IFS=';' 
-read -ra fields <<< "$@"
-
-divide_saperate() {
-fields_length=${#fields[@]}
+divide_separate() {
+fields_length=${#text_file[@]}
 for (( i = 0; i < fields_length; i++)); do
-    echo "field $((i + 1)) : ${fields[i]}"
+    echo "field $((i + 1)) : ${text_file[i]}"
 done
 
 }
 
-
+main() {
+oldIFS=$IFS
+separator="$1"
+text_file="$2" 
+IFS="$separator"
+read -ra text_file < "$text_file"  
+divide_separate "$text_file"
 IFS="$oldIFS" 
-divide_saperate
+}
+main "$1" "$2"
