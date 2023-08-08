20e5b2c8b24803e96591eb83d15a216c3add7e03
MiriamGoldin123 <miriam.goldin@grunitech.com>
2023-08-08 14:00:02 +0300

    arrange ex6.sh with functions

--- Exercise_6/ex6.sh
+++ Exercise_6/ex6.sh
@@ -2,6 +2,25 @@
 
 declare -gA USERS
 
+make2Darray() {
+  idx=1
+  while IFS=: read -r username password userid groupid user_info homedir shell; do
+    declare_users "$idx" "$username" "$userid" "$groupid" "$user_info" "$homedir" "$password"
+    ((idx++))
+  done < "$users_file"
+}
+
+print_users() {
+   # Display user details header
+   printf "%-7s %-25s %-10s %-10s %-35s %-30s %-15s\n" "Index" "Username" "UserID" "GroupID" "User Info" "Home Directory" "Password"
+
+   for ((i = 1; i < idx; i++)); do
+     printf "%-7s %-25s %-10s %-10s %-35s %-30s %-15s\n" "$i" \
+       "${USERS[$i,username]}" "${USERS[$i,userid]}" "${USERS[$i,groupid]}" \
+       "${USERS[$i,user_info]}" "${USERS[$i,homedir]}" "${USERS[$i,password]}"
+   done
+}
+
 declare_users() {
   row=$1
   username=$2
@@ -19,35 +38,28 @@ declare_users() {
   USERS["$row,password"]=$password
 }
 
-# Check if the script is run with superuser (sudo) privileges
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script requires superuser (sudo) privileges. Please run it as a superuser."
-  exit 1
-fi
+check_validation() {
+  # Check if the script is run with superuser (sudo) privileges
+  if [ "$(id -u)" -ne 0 ]; then
+    echo "This script requires superuser (sudo) privileges. Please run it as a superuser."
+    exit 1
+  fi
+
+  # Check if the system users file exists
+  if [ ! -f "$users_file" ]; then
+    echo "The system users file $users_file does not exist."
+    exit 1
+  fi
+}
 
 # Define the path to the system users file
 users_file="/etc/passwd"
 
-# Check if the system users file exists
-if [ ! -f "$users_file" ]; then
-  echo "The system users file $users_file does not exist."
-  exit 1
-fi
+# check validation of sudo and check user's file 
+check_validation
 
-# Display user details header
-printf "%-7s %-25s %-10s %-10s %-35s %-30s %-15s\n" "Index" "Username" "UserID" "GroupID" "User Info" "Home Directory" "Password"
+# Puts all users into 2D array
+make2Darray 
 
-idx=1
-
-while IFS=: read -r username password userid groupid user_info homedir shell; do
-  declare_users "$idx" "$username" "$userid" "$groupid" "$user_info" "$homedir" "$password"
-  ((idx++))
-done < "$users_file"
-
-# Display user details!
-for ((i = 1; i < idx; i++)); do
-  printf "%-7s %-25s %-10s %-10s %-35s %-30s %-15s\n" "$i" \
-    "${USERS[$i,username]}" "${USERS[$i,userid]}" "${USERS[$i,groupid]}" \
-    "${USERS[$i,user_info]}" "${USERS[$i,homedir]}" "${USERS[$i,password]}"
-done
-
+# Display user details
+print_users
