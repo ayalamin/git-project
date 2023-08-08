4d16353eea7174907ff2d355cd25668462a5f4cc
rachel <rachel@ubuntu.myguest.virtualbox.org>
2023-08-08 17:54:07 +0300

    final ex1

--- Exercise_1/ex1.sh
+++ Exercise_1/ex1.sh
@@ -1,68 +1,109 @@
 #!/bin/bash
+
+
+
 #check if input is a url
+
 is_url() {
+
     input="$1"
+
  if [[ "$input" =~ ^https?:// ]]; then
+
         return 0
+
     else
-        return 1 
+
+        return 1
+
     fi
+
 }
 
 
 
 
+
+
+
+
+
 # check if there is enouf arguments from user
+
 if [ $# -lt 2 ]; then
+
     echo "Error: Not enough arguments."
+
     exit 1
+
 fi
+
+
 
 # Get the directory
+
 direction="$1"
 
+
+
 # check if directory is exists
+
 if [ ! -d "$direction" ]; then
+
     echo "Error: directory does not exist."
+
     exit 1
+
 fi
 
 
+
+
+
 for ((i = 2; i <= $#; i++)); do
+
     url="${!i}"
+
     if ! is_url "$url"; then
+
         echo "Error: Invalid URL $url."
+
     fi
+
 done
 
 
 
-mkdir downloads/
-cd downloads/
+
+
+
+
+mkdir tmp/
+
+cd tmp/
+
+
 
 for ((i = 2; i <= $#; i++)); do
+
 url="${!i}"
-   wget --no-check-certificate -P downloads/ "$url"
+
+ wget --no-check-certificate "$url"
+
 done
+
+
 
 cd ..
-downloads_zip="downloads_$(date +%Y-%m-%d)"
 
-zip -r $downloads_zip downloads/
-
-current=$(pwd)  # Store the current directory
-
-cd "$directory"  # Change to the specified directory
-
-if [ -d "downloads" ]; then
-    # Make sure destination and source directories are distinct
-    if [ "$current" != "$(pwd)" ]; then
-        mv "$current"/downloads/* downloads/
-        cd "$current"
-        rm -r downloads 
-    fi
-else 
-    cd "$current"
-    mv downloads "$directory"
+if [ ! -d "downloads" ]; then
+mkdir downloads
 fi
+cp ./tmp/* downloads/
 
+downloads_zip="downloads_$(date +%Y-%m-%d_%H-%M)"
+
+
+zip -j $downloads_zip tmp/*
+rm -r tmp
+mv "$downloads_zip.zip" $direction
